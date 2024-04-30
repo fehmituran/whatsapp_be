5e4b396cb8c61d660d5dc99c08886e235bc39c71
fehmituran <86859577+fehmituran@users.noreply.github.com>
2024-04-30 14:09:58 -0400

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-
 import app from "./app.js";
-
-
+import logger from "./configs/logger.config.js";
 
 //env variables
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}...`);
+  logger.info(`server is running on http://localhost:${PORT}...`);
 });

6f2e265940e5c38558d7f095305060b50a1b7dcf
fehmituran <86859577+fehmituran@users.noreply.github.com>
2024-04-30 13:38:43 -0400

    setup croos-env, adding middlwares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,51 @@
 import express from "express";
 
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cokkieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//dotenv config
+dotenv.config();
+
 //create express app
 const app = express();
+
+//morgan
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//Parse json request url
+app.use(express.json());
+//Parse json request body
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//enable cookie parser
+app.use(cokkieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("heloo from server apliation");
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import dotenv from "dotenv";
+
 import app from "./app.js";
 
-//dotenv config
-dotenv.config();
+
 
 //env variables
 const PORT = process.env.PORT || 8000;
